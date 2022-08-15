204e2472bf8473823c2ce3fb5de679b43ad51ff5
Harshini Kodali <vtu14790@veltech.edu.in>
2022-08-15 18:37:13 +0530

    Update countCharacters.js

--- countCharacters.js
+++ countCharacters.js
@@ -1,3 +1,3 @@
 var message=prompt("Compose your message:");
 var len=message.length;
-alert("you have written "+ len +"characters and you have left with"+(280-len));
+alert("you have written "+" "+ len +"characters and you have left with"+" "+(280-len));
