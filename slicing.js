15c28648c664c81fbb7e8c234d460bdb3bbdb548
Harshini Kodali <vtu14790@veltech.edu.in>
2022-08-15 18:57:30 +0530

    Update slicing.js

--- slicing.js
+++ slicing.js
@@ -1,3 +1,9 @@
 var mssg=prompt("Enter ur mssg");
 var slicingMssg=mssg.slice(0,280);//since I need upto 280 characters of entered text mssg
 alert(slicingMssg);
+
+
+
+ /* Or the following is also a possibility */
+
+alert(prompt("Enter ur mssg").slice(0,280));
