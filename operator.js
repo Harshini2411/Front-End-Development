ea48cab762e5f8fb268e7174ce7a6bfc340d3729
Harshini Kodali <vtu14790@veltech.edu.in>
2022-08-15 21:48:22 +0530

    Delete operator.js

--- operator.js
+++
@@ -1,4 +0,0 @@
-//dogAge to humanAge Convertor using Operators.
-var dogAge=prompt("Enter Dog Age");
-var humanAge=(dogAge-2)*4+21;
-alert("Your dog is"+" "+humanAge+" "+"years old in human years.");
