186810daa44fd5121c7a8859e663dc337ee20502
Harshini Kodali <vtu14790@veltech.edu.in>
2022-08-20 15:18:28 +0530

    Create leapYear.js

---
+++ leapYear.js
@@ -0,0 +1,22 @@
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here.    
+if(year%4 === 0 || year%400 === 0)
+{
+    console.log("Leap year.");
+}
+else if(year%100 === 0 && year%400 !== 0)
+{
+    console.log("Not leap year.");
+}
+else
+{
+    console.log("Not leap year.");
+}
+
+/**************Don't change the code below****************/    
+
+}
+isLeap(1900);
