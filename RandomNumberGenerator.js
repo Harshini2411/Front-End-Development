fddf2b35a43199e7f9845611bc1c5a7fd11cddf9
Harshini Kodali <vtu14790@veltech.edu.in>
2022-08-20 14:41:30 +0530

    Update RandomNumberGenerator.js

--- RandomNumberGenerator.js
+++ RandomNumberGenerator.js
@@ -4,4 +4,10 @@ var randomNumber=Math.random();//generates random number b/w 0-0.999999999999999
 randomNumber=Math.floor(randomNumber*100);//to calculate on %, it goes upto 100
 randomNumber=randomNumber+1;//it also includes 0,to eliminate that +1
 // console.log(n);
-alert("Your Love Score is "+randomNumber+"%");
+if(randomNumber >=70){
+alert("Your Love Score is "+randomNumber+"%"+"You both are made for each other");
+}
+else
+{
+  alert("Your Love Score is "+randomNumber+"%");
+        }
